84ef48127090a39193d10bafb003576855e7f443
Milen Shark <milen.shark@gmail.com>
2021-03-06 19:27:27 +0200

    Resolving problem one - numbers from 1 to N

--- src/com/company/ProblemOneNumbersFromOneToN.java
+++ src/com/company/ProblemOneNumbersFromOneToN.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class ProblemOneNumbersFromOneToN {
     public static void main(String[] args) {
 
@@ -7,7 +9,17 @@ public class ProblemOneNumbersFromOneToN {
 //        Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n,
 //        on a single line, separated by a space.
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a positive integer value: ");
+        int n = Integer.parseInt(scanner.nextLine());
 
+        while (n <= 0) {
+            System.out.print("Please enter a positive integer value: ");
+            scanner.nextLine();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
