e69385654e1a4986250f4a617ad3542322128d94
Milen Shark <milen.shark@gmail.com>
2021-03-06 19:16:02 +0200

    Initial commit

---
+++ src/com/company/ProblemOneNumbersFromOneToN.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ProblemOneNumbersFromOneToN {
+    public static void main(String[] args) {
+
+//        Problem 1. Numbers from 1 to N
+//        Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n,
+//        on a single line, separated by a space.
+
+        
+
+    }
+}
